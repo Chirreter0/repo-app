21ccb9a20d844a10d9c2ffc003612b35da8c5ccb
Nicolas Leguizamo <nicolasleguizamo.es@unitropico.edu.co>
2024-10-25 13:07:51 -0500

    correcciones

--- vendor/orchid/platform/resources/views/auth.blade.php
+++ vendor/orchid/platform/resources/views/auth.blade.php
@@ -6,7 +6,7 @@
         <div class="form-signin h-full min-vh-100 d-flex flex-column justify-content-center">
 
             <a class="d-flex justify-content-center mb-4 p-0 px-sm-5" href="{{Dashboard::prefix()}}">
-                @includeFirst([config('platform.template.header'), 'platform::header'])</a>
+                @includeFirst([config('platform.template.header'), 'platform::header'])Login</a>
 
             <div class="row justify-content-center">
                 <div class="col-md-10 col-lg-6 col-xxl-5 px-md-5">
